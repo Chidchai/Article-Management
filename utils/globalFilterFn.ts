a5ebee4450b22ad0e4038e34a508eeb10b63b267
Chidchai <chidchai.cs38@gmail.com>
2025-07-20 23:48:54 +0700

    checkpoint

--- utils/globalFilterFn.ts
+++
@@ -1,7 +0,0 @@
-export function globalFilterFn(row: any, columnId: string, filterValue: string) {
-    return row
-        .getAllCells()
-        .some((cell: any) =>
-            String(cell.getValue()).toLowerCase().includes(filterValue.toLowerCase())
-        );
-}
